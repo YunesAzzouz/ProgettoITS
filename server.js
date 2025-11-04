b20b1f355b128b86d89ebfa55601f453213eb3f0
Y Azzouz <yunesnetwork@gmail.com>
2025-11-04 16:49:55 +0100

    Roba inutile rimossa

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ const express = require("express");
 const path = require("path");
 const { MongoClient } = require("mongodb");
 const bcrypt = require("bcrypt");
-const cors = require("cors");
-const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
 
-app.use(cors());
-app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "front-end")));
